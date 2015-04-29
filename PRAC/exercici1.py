549b5998e7fdc3af2f964d02f1a19375d672b4a0
droca <droca@netquest.com>
2015-04-29 18:31:02 +0200

    refactor of implementation

--- PRAC/exercici1.py
+++ PRAC/exercici1.py
@@ -6,58 +6,19 @@ from utils import *
 from math import exp
 
 OBSERVATIONS_NUMBER = 1000
-GENERATION_CORRECT = 'Correctly generated'
-GENERATION_ERROR = 'Error in generation'
 
 # --- Apartat a ---#
 
-# Generate the values for the variable x1
-print('Begin the generation of the x1 variable: ',end="")
-try:
-	x1 = generateVariableObs(OBSERVATIONS_NUMBER, 0.5, 0.1)
-	print(GENERATION_CORRECT)
-except:
-	print (GENERATION_ERROR)
-	raise
-
-# Generate the values for the variable x2
-print('Begin the generation of the x2 variable: ',end="")
-try:
-	x2 = generateVariableObs(OBSERVATIONS_NUMBER, 4.5, 0.6)
-	print (GENERATION_CORRECT)
-except:
-	print (GENERATION_ERROR)
-	raise
-
-# Calculate the values for the variable x3
-print('Begin the generation of the x3 variable: ',end="")
-try:
-	x3 = [i*3 for i in x1]
-	print(GENERATION_CORRECT)
-except:
-	print (GENERATION_ERROR)
-	raise
-
-# Calculate the values for the variable x4
-print('Begin the generation of the x4 variable: ',end="")
-try:
-	x4=ndarray((1000,),int)
-	for i in range(OBSERVATIONS_NUMBER-1):
-		x4[i] = exp(x1[i])*2+x2[i]
-	print (GENERATION_CORRECT)
-except:
-	print (GENERATION_ERROR)
-	raise
+# Generate the values for the variables x1..x4
+x1 = createVariable('x1', OBSERVATIONS_NUMBER, 0.5, 0.1)
+x2 = createVariable('x2', OBSERVATIONS_NUMBER, 4.5, 0.6)
+x3 = createVariableEscalarMultiplication('x3', OBSERVATIONS_NUMBER, 3, x1)
+x4 = createVariableExpAndSum('x4', OBSERVATIONS_NUMBER, 2, x1, x2)
 
 print()
 print('All arrays correctly generated, proceeding to build the matrix')
 
 # Build the matrix A, with the data of the 4 variables
-print('Begin building the matrix with all the arrays: ',end="")
-try:
-	A = buildMatrix(x1, x2, x3, x4)
-	print (GENERATION_CORRECT)
-except:
-	print(GENERATION_ERROR)
-	raise
+A = buildMatrix(x1, x2, x3, x4)
+
 
--- PRAC/utils.py
+++ PRAC/utils.py
@@ -3,17 +3,59 @@
 # PRAC IAA - UOC 2015
 
 from numpy import *
+from math import exp
 
-# Generation a number of random values that will have the mean and stdDev indicated
-def generateVariableObs(number, mean, stdDev):
-	return 	random.normal(mean, stdDev, number)
+# Messages definition
+GENERATION_INIT = "'Begin the generation of the %s variable: ' % name"
+GENERATION_CORRECT = 'Correctly generated'
+GENERATION_ERROR = 'Error in generation'
+
 
 # Builds a matrix concatenating the 4 arrays
 def buildMatrix(ar1, ar2, ar3, ar4):
-	return concatenate((ar1, ar2, ar3, ar4),0)
+	print('Begin building the matrix with all the arrays: ',end="")
+	try:
+		m = concatenate((ar1, ar2, ar3, ar4),0)
+		print (GENERATION_CORRECT)
+	except:
+		print(GENERATION_ERROR)
+		raise
+	return m
 
+# Create the standard variable, normal distribution
+def createVariable(name, number, mean, stdDev):
+	print(GENERATION_INIT,end="")
+	try:
+		x = random.normal(mean, stdDev, number)
+		print(GENERATION_CORRECT)
+	except:
+		print (GENERATION_ERROR)
+		raise
+	return x
 
+# Create the variable using a lineal function, escalar product
+def createVariableEscalarMultiplication(name, number, escalar, array):
+	print(GENERATION_INIT,end="")
+	try:
+		x = [i*escalar for i in array]
+		print(GENERATION_CORRECT)
+	except:
+		print (GENERATION_ERROR)
+		raise
+	return x
 
+# Create the variable using a lineal function, using two arrays and exponentials
+def createVariableExpAndSum(name, number, constant, array1, array2):
+	print(GENERATION_INIT,end="")
+	try:
+		x=ndarray((number,),int)
+		for i in range(number-1):
+			x[i] = exp(array1[i])*constant+array2[i]
+		print (GENERATION_CORRECT)
+	except:
+		print (GENERATION_ERROR)
+		raise
+	return x
 
 
 
